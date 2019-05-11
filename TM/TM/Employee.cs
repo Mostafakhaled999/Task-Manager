0a661c88d6a53e052d190a90f5eeb8a6e44e812e
MostafaKhaled999 <mostafa.khaled.std@cis.asu.edu.eg>
2019-05-12 00:39:30 +0200

    first

---
+++ TM/TM/Employee.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
+namespace TM
+{
+    abstract class Employee
+    {
+        protected int id;
+       public static int noOfemp = 0;
+        protected string name, password;
+        
+        public Employee(int i,string n,string p)
+        {
+            id = i;
+            name = n;
+            password = p;
+        }
+      public  Employee()
+        {
+            id = 0;
+            name = null;
+            password =null;
+        }
+        public abstract bool register();
+        public abstract bool login(int i,string pass);
+        public abstract bool comment();
+        public abstract bool attach();
+        public abstract bool forward();
+        public abstract bool download();
+
+    }
+    class User : Employee
+    {
+        
+       public User() : base()
+        {
+
+        }
+        public User(int i,string s,string p) : base(i,s,p)
+        {
+
+        }
+        public override bool attach()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool comment()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool download()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool forward()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool login(int i,string pass)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("Employees.xml");
+            XmlNodeList list = doc.GetElementsByTagName("user");
+
+            for (int a   = 0; a < list.Count; a++)
+            {
+                XmlNodeList child = list[a].ChildNodes;
+                if (i.Equals(int.Parse(child[0].InnerText))&&pass.Equals(child[2].InnerText))
+                {
+                    id = int.Parse(child[0].InnerText);
+                    name = child[1].InnerText;
+                    password = child[2].InnerText;
+                    return true;
+                }
+                
+
+            }
+            return false;
+
+        }
+
+        public override bool register()
+        {
+            if (!File.Exists("Employees.xml"))
+            {
+                XmlWriter writer = XmlWriter.Create("Employees.xml");
+                writer.WriteStartDocument();
+                writer.WriteStartElement("Emloyees");
+
+                writer.WriteStartElement("Users");
+
+                writer.WriteStartElement("user");
+
+                writer.WriteStartElement("ID");
+                writer.WriteString(id.ToString());
+                writer.WriteEndElement();
+
+                writer.WriteStartElement("Name");
+                writer.WriteString(name);
+                writer.WriteEndElement();
+
+                writer.WriteStartElement("Password");
+                writer.WriteString(password);
+                writer.WriteEndElement();
+
+                writer.WriteEndElement();
+                writer.WriteEndElement();
+                writer.WriteEndElement();
+                writer.WriteEndDocument();
+                writer.Close();
+
+                return true;
+            }
+            else
+            {
+                XmlDocument doc = new XmlDocument();
+                XmlElement user = doc.CreateElement("user");
+
+                XmlElement node = doc.CreateElement("ID");
+                node.InnerText = id.ToString();
+                user.AppendChild(node);
+
+                node = doc.CreateElement("Name");
+                node.InnerText = name;
+                user.AppendChild(node);
+
+                node = doc.CreateElement("Password");
+                node.InnerText = password;
+                user.AppendChild(node);
+
+                doc.Load("Employees.xml");
+                XmlElement root = doc.DocumentElement["Users"];
+                root.AppendChild(user);
+
+                doc.Save("Employees.xml");
+                return true;
+            }
+        }
+    }
+
+}
--- TM/TM/Form1.cs
+++ TM/TM/Form1.cs
@@ -10,11 +10,44 @@ using System.Windows.Forms;
 
 namespace TM
 {
+     
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
+            
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            User user = new User(int.Parse(textBox1.Text),textBox2.Text,textBox3.Text);
+            
+            if (user.register())
+            {
+                Employee.noOfemp++;
+                MessageBox.Show("User added successfuly");
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+            }
+           
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            User user = new User();
+            if (user.login(int.Parse(textBox4.Text),textBox6.Text))
+            {
+                MessageBox.Show("Login successfuly :D");
+            }
+            else
+            {
+                MessageBox.Show("<<<Login Faild>>>");
+            }
         }
     }
 }
